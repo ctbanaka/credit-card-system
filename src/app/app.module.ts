8fbded6ae5e0d0fa5c419cb69703d8f94b02aa4b
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-06-18 12:46:51 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddRegistrtionComponent } from './add-registrtion/add-registrtion.component';
+import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRegistrtionComponent
+    AddRegistrtionComponent,
+    WelcomeComponentComponent
   ],
   imports: [
     BrowserModule,
