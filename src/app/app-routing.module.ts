c2ac701bd93bbb930c1a5d7537c8fa7e34117f24
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-06-21 11:20:44 +0530

    register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,18 @@ import { GetallAddressComponent } from './getall-address/getall-address.componen
 
 import { WelcomeComponent } from './welcome/welcome-component';
 import { AdminComponent } from './admin/admin.component';
+import { AddRegistrtionComponent } from './add-registrtion/add-registrtion.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path: 'welcome', component:WelcomeComponent},
   { path: ' ',redirectTo: 'welcome',pathMatch:'full'},
-  {path : 'admin',component:AdminComponent,
-  children:[{
-       path:'getall-registration',component:GetallRegistrationComponent},
-    ]},
- 
-
-
+  {path : 'admin',component:AdminComponent},
+  {path:'registration', component:RegistrationComponent},
+  
+  {path: 'register', component:AddRegistrtionComponent},
+  {path:'getalladdress' , component:GetallAddressComponent},
   { path: '**',redirectTo: 'welcome',pathMatch:'full'},
-  {path:'getalladdress' , component:GetallAddressComponent}
 
 ];
 
