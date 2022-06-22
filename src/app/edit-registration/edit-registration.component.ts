d0ee5084f1e1cc2c5896fcd93e613e8cfa8b3b28
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2022-06-23 00:48:01 +0530

    update

--- src/app/edit-registration/edit-registration.component.ts
+++ src/app/edit-registration/edit-registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationService } from '../registration.service';
 
 @Component({
   selector: 'app-edit-registration',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-registration.component.css']
 })
 export class EditRegistrationComponent implements OnInit {
-
-  constructor() { }
+  editForm!:FormGroup;
+  constructor(private service:RegistrationService,private router:Router) { }
 
   ngOnInit(): void {
+    this.editForm = new FormGroup({
+      userId: new FormControl(),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      dateOfBirth:new FormControl('',Validators.required),
+      phoneNo:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.email),
+      password: new FormControl ('', Validators.required)
+  });
+
+  }
+ 
+  editRegistration():void{
+    this.service.editRegistration(this.editForm.value)
+    .subscribe(data =>{console.log(data);
+      this.router.navigate(['welcome']);
+    })
+
+    
+  }
+
+  onSubmit(){
+    this.editRegistration();
   }
 
 }
